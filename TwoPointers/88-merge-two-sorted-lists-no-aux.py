d8cf3353641ae0f4cbd378b2a7892d2a952bdddd
esousacosta <victoremanoel23@gmail.com>
2025-04-30 17:57:28 +0200

    Add in-place sol for ltc 88

--- TwoPointers/88-merge-two-sorted-lists-no-aux.py
+++ TwoPointers/88-merge-two-sorted-lists-no-aux.py
@@ -3,6 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        """"
+        Trick: start filling the list from the end to the beginning
+        """
         if len(nums1) == 1:
             nums1[0 : len(nums2)] = nums2
             return
@@ -15,16 +18,15 @@ class Solution:
 
         while l >= 0 and r >= 0:
             if nums1[l] >= nums2[r]:
-                nums1[i] = aux[l]
+                nums1[i] = nums1[l]
                 l -= 1
             else:
                 nums1[i] = nums2[r]
                 r -= 1
             i -= 1
 
-        if l == 0:
+        # if nums2 runs out of elements first,
+        # the remaining elements in nums1 are already
+        # sorted and in the correct place
+        if l < 0:
             nums1[0 : i + 1] = nums2[0 : r + 1]
-        elif r == 0:
-            # we don't want to include the zeros at the end of the
-            # first array.
-            nums1[0 : i + 1] = aux[0 : l + 1]
