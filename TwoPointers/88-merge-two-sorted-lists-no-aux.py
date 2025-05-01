be4279a717f2266400952e0f226e206eba44ac3d
esousacosta <victoremanoel23@gmail.com>
2025-05-01 17:56:15 +0200

    Fix ltc 88 merge sorted lists to remove unnecessary code and add better path

--- TwoPointers/88-merge-two-sorted-lists-no-aux.py
+++ TwoPointers/88-merge-two-sorted-lists-no-aux.py
@@ -6,27 +6,25 @@ class Solution:
         """"
         Trick: start filling the list from the end to the beginning
         """
-        if len(nums1) == 1:
-            nums1[0 : len(nums2)] = nums2
+        if len(nums1) == len(nums2):
+            nums1[:] = nums2
             return
         elif len(nums2) == 0:
             return
 
-        i = m + n - 1
-        r = n - 1
+        target = m + n - 1
         l = m - 1
+        r = n - 1
 
         while l >= 0 and r >= 0:
             if nums1[l] >= nums2[r]:
-                nums1[i] = nums1[l]
+                nums1[target] = nums1[l]
                 l -= 1
             else:
-                nums1[i] = nums2[r]
+                nums1[target] = nums2[r]
                 r -= 1
-            i -= 1
 
-        # if nums2 runs out of elements first,
-        # the remaining elements in nums1 are already
-        # sorted and in the correct place
+            target -= 1
+
         if l < 0:
-            nums1[0 : i + 1] = nums2[0 : r + 1]
+            nums1[: target + 1] = nums2[: r + 1]
