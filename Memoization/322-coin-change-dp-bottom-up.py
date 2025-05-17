01658434e7c9c1c4685f6d52113cacc7975b89ad
esousacosta <victoremanoel23@gmail.com>
2025-05-17 20:44:09 +0200

    Change coin change (ltc 322) to use pre-init array

--- Memoization/322-coin-change-dp-bottom-up.py
+++ Memoization/322-coin-change-dp-bottom-up.py
@@ -1,26 +1,24 @@
-import sys
+from sys import maxsize
+
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-
-        memo = {}
+        memo = [-1] * (amount + 1)
         memo[0] = 0
 
-        for i in range(1, amount + 1):
-            memo[i] = -1
-            currCoins = 0
+        for i in range(amount + 1):
             minCoins = sys.maxsize
-            for coin in coins:
-                if coin <= i:
-                    if memo[i - coin] == -1:
-                        continue
-                    currCoins = 1 + memo[i - coin]
-                    if currCoins < minCoins:
-                        minCoins = currCoins
+            for c in coins:
+                if c > i:
+                    continue
+                subproblem = i - c
+                if memo[subproblem] == -1:
+                    continue
+                currCoins = 1 + memo[subproblem]
+                if currCoins < minCoins:
+                    minCoins = currCoins
+
             if minCoins != sys.maxsize:
                 memo[i] = minCoins
-            else:
-                memo[i] = -1
 
-        return memo[amount]        
+        return memo[amount]
