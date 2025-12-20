4c6decadbbfa54e52d031197dc7ea480c86eefc8
esousacosta <victoremanoel23@gmail.com>
2025-12-20 22:32:55 +0100

    Add Java sol for prod except self (ltc 238) no div

---
+++ HasingAndArrays/ProductArrayExceptSelf/238-product-array-except-self-no-div.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+
+        output[0] = 1;
+
+        // left to right (prefix)
+        for (int i = 1; i < nums.length; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+
+        // Right to left (suffix)
+        int suffixProd = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            output[i] = output[i] * suffixProd;
+            suffixProd *= nums[i];
+        }
+
+        return output;
+    }
+}  
