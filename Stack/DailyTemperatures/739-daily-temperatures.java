e51c74a33debb6c6b007150b8be8a85ad06da999
esousacosta <victoremanoel23@gmail.com>
2025-12-24 16:58:27 +0100

    improve java sol for ltc 739 by preallocating the stack sixe

--- Stack/DailyTemperatures/739-daily-temperatures.java
+++ Stack/DailyTemperatures/739-daily-temperatures.java
@@ -4,7 +4,7 @@ import java.util.Deque;
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-        Deque<Integer> stack = new ArrayDeque<>();
+        Deque<Integer> stack = new ArrayDeque<>(n);
         int[] out = new int[n];
 
         for (int i = 0; i < n; i++) {
