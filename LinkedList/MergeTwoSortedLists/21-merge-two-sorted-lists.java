34309b69a70b4eefa3eae206f35c28d660950685
esousacosta <victoremanoel23@gmail.com>
2025-12-28 14:39:06 +0100

    simplify java sol for ltc 21

--- LinkedList/MergeTwoSortedLists/21-merge-two-sorted-lists.java
+++ LinkedList/MergeTwoSortedLists/21-merge-two-sorted-lists.java
@@ -1,72 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) {
-            return list2;
-        } else if (list2 == null) {
-            return list1;
-        }
-        
-        ListNode head = null;
-        ListNode tip = null;
+        // Dummy node to simplify edge cases
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
 
-        if (list1.val <= list2.val) {
-            head = list1;
-            tip = head;
-            list1 = list1.next;
-            if (list1 == null) {
-                head.next = list2;
-                return head;
-            }
-        } else {
-            head = list2;
-            tip = head;
-            list2 = list2.next;
-            if (list2 == null) {
-                head.next = list1;
-                return head;
-            }
-        }
-
+        // Traverse both lists
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
-                if (head.next == null) {
-                    head.next = list1;
-                }
-
-                tip.next = list1;
-                tip = tip.next;
+                current.next = list1;
                 list1 = list1.next;
-
-                if (list1 == null) {
-                    tip.next = list2;
-                    return head;
-                }
             } else {
-                if (head.next == null) {
-                    head.next = list2;
-                }
-
-                tip.next = list2;
-                tip = tip.next;
+                current.next = list2;
                 list2 = list2.next;
-
-                if (list2 == null) {
-                    tip.next = list1;
-                    return head;
-                }
             }
+            current = current.next;
         }
 
-        return head;
+        // Attach remaining nodes
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
     }
 }
